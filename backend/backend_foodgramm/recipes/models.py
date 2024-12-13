1bf54747682f266c914a0dd1a7cd50e54b2533db
Александр Игошев <aigoshev69@mail.ru>
2024-12-13 15:29:09 +0500

    1 iteration

--- backend/backend_foodgramm/recipes/models.py
+++ backend/backend_foodgramm/recipes/models.py
@@ -1,10 +1,10 @@
 import re
 
 from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.core.exceptions import ValidationError
 from django.core.validators import MinValueValidator
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
 MAX_LENGTH_USERNAME = 150
 MAX_LENGTH_EMAIL = 150
