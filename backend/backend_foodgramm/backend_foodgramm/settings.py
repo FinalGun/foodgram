729bb20df6daf795006f73c80229c8d94619a462
Александр Игошев <aigoshev69@mail.ru>
2024-12-14 16:41:34 +0500

    1 iteration

--- backend/backend_foodgramm/backend_foodgramm/settings.py
+++ backend/backend_foodgramm/backend_foodgramm/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = "backend_foodgramm.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+'''
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
@@ -92,7 +92,7 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
-'''
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
